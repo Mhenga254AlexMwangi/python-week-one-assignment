9f206a2d598109ac6b933ef1c5701805aca6594d
AlexMwangi <147475710+Mhenga254AlexMwangi@users.noreply.github.com>
2025-08-08 21:20:43 +0300

    Add files via upload
    second week assignment

---
+++ week2  my_list.py
@@ -0,0 +1,18 @@
+my_list=[]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(2,15)
+print(my_list)
+add=50,60,70
+my_list.extend(add)
+print(my_list)
+my_list.pop(-1)
+print(my_list)
+my_list.sort ()
+print(my_list)
+index=my_list.index(30)
+print("the index is:",index)
+
